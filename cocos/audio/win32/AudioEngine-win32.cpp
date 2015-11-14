6daae2f1a8b121e77d7d65795f2156ff67bde81f
initrdmk <initrdmk@gmail.com>
2015-11-14 20:04:49 +0800

    add audio pitch support for win32

--- cocos/audio/win32/AudioEngine-win32.cpp
+++ cocos/audio/win32/AudioEngine-win32.cpp
@@ -258,6 +258,7 @@ int AudioEngineImpl::play2d(const std::string &filePath ,bool loop ,float volume
     player->_alSource = alSource;
     player->_loop = loop;
     player->_volume = volume;
+    player->_pitch = 1.0f;
     audioCache->addCallbacks(std::bind(&AudioEngineImpl::_play2d,this,audioCache,_currentAudioID));
     
     _alSourceUsed[alSource] = true;
@@ -303,6 +304,21 @@ void AudioEngineImpl::setVolume(int audioID,float volume)
     if (player._ready) {
         alSourcef(_audioPlayers[audioID]._alSource, AL_GAIN, volume);
         
+        auto error = alGetError();
+        if (error != AL_NO_ERROR) {
+            log("%s: audio id = %d, error = %x\n", __FUNCTION__,audioID,error);
+        }
+    }
+}
+
+void AudioEngineImpl::setPitch(int audioID, float pitch)
+{
+    auto& player = _audioPlayers[audioID];
+    player._pitch = pitch;
+
+    if (player._ready) {
+        alSourcef(_audioPlayers[audioID]._alSource, AL_PITCH, pitch);
+
         auto error = alGetError();
         if (error != AL_NO_ERROR) {
             log("%s: audio id = %d, error = %x\n", __FUNCTION__,audioID,error);
